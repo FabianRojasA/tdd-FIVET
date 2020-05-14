994b56c7f9a2cf52255528d0f92b03e7072eb68a
Fabian Rojas <fnra027@gmail.com>
2020-05-14 11:38:03 -0400

    feat(Test): create test for repository of ficha

--- src/test/java/cl/ucn/disc/pdbp/tdd/dao/StorageTest.java
+++ src/test/java/cl/ucn/disc/pdbp/tdd/dao/StorageTest.java
@@ -24,7 +24,11 @@
 
 package cl.ucn.disc.pdbp.tdd.dao;
 
+import cl.ucn.disc.pdbp.tdd.model.Ficha;
 import cl.ucn.disc.pdbp.tdd.model.Persona;
+import cl.ucn.disc.pdbp.tdd.model.Sexo;
+import cl.ucn.disc.pdbp.tdd.model.TipoPaciente;
+import cl.ucn.disc.pdbp.tdd.utils.Entity;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
@@ -35,6 +39,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -131,6 +136,51 @@ public final class StorageTest {
             //TODO agregar mas test
 
         }catch (IOException | SQLException e){
+            throw new RuntimeException();
+        }
+    }
+
+    @Test
+    public void testRespositoryFicha(){
+        //The database to use (in RAM memory)
+        String databaseUrl = "jdbc:h2:mem:fivet_db";
+
+        try (ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl)){
+            //Create the table in backend
+            TableUtils.createTableIfNotExists(connectionSource, Persona.class);
+            TableUtils.createTableIfNotExists(connectionSource, Ficha.class);
+
+            Persona duenio = new Persona("andrea", "contreras", "192047692","email@gmail.com");
+            Repository<Ficha, Long> repositoryFicha = new RepositoryOrmLite<>(connectionSource, Ficha.class);
+
+            {
+                //1. Crear la Persona desde un Repository
+
+                if (!new RepositoryOrmLite<Persona, Long>(connectionSource, Persona.class).create(duenio)) {
+                    Assertions.fail("Can't insert Persona!");
+                }
+
+                //2. Instanciar una ficha
+                Ficha ficha = new Ficha(123, "firulais", "canino", ZonedDateTime.now(), "yorkshire", Sexo.MACHO, "cafe con plomo", TipoPaciente.INTERNO, duenio);
+
+                //3. Crear la ficha
+                if (!repositoryFicha.create(ficha)) {
+                    Assertions.fail("Falla en crear la ficha");
+                }
+            }
+
+            //FIXME arreglar ficha
+            //4. Obtener una ficha y revisar si sus atributos son distintos de null
+            Ficha ficha = repositoryFicha.findById((long) 1);
+            Assertions.assertNotNull(ficha,"Ficha was null");
+            Assertions.assertNotNull(ficha.getDuenio(),"Ficha was null");
+            Assertions.assertNotNull(ficha.getDuenio().getRut(),"Rut del Duenio de Ficha was null");
+            Assertions.assertNotNull(ficha.getFechaNacimiento(),"FechaNacimiento was null");
+
+            log.debug("Ficha{}.", Entity.toString(ficha));
+
+
+        }catch (SQLException | IOException exception){
             throw new RuntimeException();
         }
     }
