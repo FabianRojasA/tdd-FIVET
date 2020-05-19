ae664287ce18eb75cfedfce9601835dc1a615c35
Fabian Rojas <fnra027@gmail.com>
2020-05-19 10:21:40 -0400

    style(Persona): add java google style format

--- src/main/java/cl/ucn/disc/pdbp/tdd/model/Persona.java
+++ src/main/java/cl/ucn/disc/pdbp/tdd/model/Persona.java
@@ -1,4 +1,3 @@
-
 /*
  * MIT License
  *
@@ -28,103 +27,102 @@ package cl.ucn.disc.pdbp.tdd.model;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
-/**
- *
- */
 @DatabaseTable(tableName = "persona")
 public class Persona {
 
-    /**
-     * The id: Primary key and autoincrement
-     */
-    @DatabaseField(generatedId = true)
-    private long id;
+  /** The id: Primary key and autoincrement. */
+  @DatabaseField(generatedId = true)
+  private long id;
 
-    /**
-     * Nombre de la persona
-     */
-    @DatabaseField(canBeNull = false)
-    private String nombre;
+  /** Nombre de la persona. */
+  @DatabaseField(canBeNull = false)
+  private String nombre;
 
-    /**
-     * Apellido de la persona
-     */
-    @DatabaseField(canBeNull = false)
-    private String apellido;
+  /** Apellido de la persona. */
+  @DatabaseField(canBeNull = false)
+  private String apellido;
 
-    /**
-     * Rut de la persona
-     */
-    @DatabaseField(canBeNull = false, index = true)
-    private String rut;
+  /** Rut de la persona. */
+  @DatabaseField(canBeNull = false, index = true)
+  private String rut;
 
-    @DatabaseField(canBeNull = false)
-    private String email;
+  /** Correo electronico de la persona. */
+  @DatabaseField(canBeNull = false)
+  private String email;
 
-    /**
-     * Empty Constructor
-     */
-    Persona() {
-        //Nothing here
+  /** Empty Constructor. */
+  Persona() {}
+
+  /**
+   * Constructor de una persona.
+   *
+   * @param nombre a usar
+   * @param apellido a usar
+   * @param rut valido
+   */
+  public Persona(String nombre, String apellido, String rut, String email) {
+    if (nombre == null) {
+      throw new NullPointerException("Nombre invalido");
+    }
+    if (apellido == null) {
+      throw new NullPointerException("Apellido invalido");
+    }
+    if (rut == null) {
+      throw new NullPointerException("Rut invalido");
+    }
+    if (!Validation.isRutValid(rut)) {
+      throw new RuntimeException();
     }
 
-    /**
-     * Constructor de una persona
-     * @param nombre a usar
-     * @param apellido a usar
-     * @param rut valido
-     */
-    public Persona(String nombre, String apellido, String rut, String email) {
-        if (nombre == null){ throw new NullPointerException("Nombre invalido");}
-        if (apellido == null) {throw new NullPointerException("Apellido invalido");}
-        if (rut == null) {throw new NullPointerException("Rut invalido");}
-        if (!Validation.isRutValid(rut)) { throw new RuntimeException();}
-
-        if (nombre.length() <2 ){ throw new RuntimeException("Nombre invalido");}
-        if (apellido.length() <3 ){ throw new RuntimeException("Apellido invalido");}
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.rut = rut;
-        this.email = email;
+    if (nombre.length() < 2) {
+      throw new RuntimeException("Nombre invalido");
     }
-
-    /**
-     * Retorna el nombre de la persona
-     * @return nombre
-     */
-    public String getNombre() {
-        return this.nombre;
+    if (apellido.length() < 3) {
+      throw new RuntimeException("Apellido invalido");
     }
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.rut = rut;
+    this.email = email;
+  }
 
-    /**
-     * Retorna el apellido de la persona
-     * @return apellido
-     */
-    public String getApellido() {
-        return this.apellido;
-    }
+  /**
+   * Retorna el nombre de la persona.
+   * @return nombre
+   */
+  public String getNombre() {
+    return this.nombre;
+  }
 
-    /**
-     * Retorta un string que concatena el nombre y apellido de la persona
-     * @return nombre espacio apellido
-     */
-    public String getNombreApellido() {
-        return this.nombre + " " + this.apellido;
-    }
+  /**
+   * Retorna el apellido de la persona.
+   * @return apellido
+   */
+  public String getApellido() {
+    return this.apellido;
+  }
 
-    /**
-     * Retorna el rut de la persona
-     * @return rut
-     */
-    public String getRut() {
-        return rut;
-    }
+  /**
+   * Retorna un string que concatena el nombre y apellido de la persona.
+   * @return nombre espacio apellido
+   */
+  public String getNombreApellido() {
+    return this.nombre + " " + this.apellido;
+  }
 
-    /**
-     * Retorna el id
-     * @return id
-     */
-    public long getId() {
-        return id;
-    }
+  /**
+   * Retorna el rut de la persona.
+   * @return rut
+   */
+  public String getRut() {
+    return rut;
+  }
+
+  /**
+   * Retorna el id.
+   * @return id
+   */
+  public long getId() {
+    return id;
+  }
 }
