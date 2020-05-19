62dd2347c057acb2a0de9cbed780fc7a58c3ebdb
Fabian Rojas <fnra027@gmail.com>
2020-05-19 10:03:25 -0400

    style(Control): add java google style format

--- src/main/java/cl/ucn/disc/pdbp/tdd/model/Control.java
+++ src/main/java/cl/ucn/disc/pdbp/tdd/model/Control.java
@@ -29,43 +29,29 @@ import java.time.ZonedDateTime;
 
 public class Control {
 
-    /**
-     * Fecha del control
-     */
-    private final ZonedDateTime fecha;
+  /** Fecha del control. */
+  private final ZonedDateTime fecha;
 
-    /**
-     * Fecha del proximo control
-     */
-    private final ZonedDateTime fechaProximo;
+  /** Fecha del proximo control. */
+  private final ZonedDateTime fechaProximo;
 
-    /**
-     * Temperatura del paciente en grados celcius
-     */
-    private final double temperatura;
+  /** Temperatura del paciente en grados celcius. */
+  private final double temperatura;
 
-    /**
-     * Peso del paciente en kilos
-     */
-    private final double peso;
+  /** Peso del paciente en kilos. */
+  private final double peso;
 
-    /**
-     * Altura del paciente en centimetros
-     */
-    private final double altura;
+  /** Altura del paciente en centimetros. */
+  private final double altura;
 
-    /**
-     * Diagnostico del paciente
-     */
-    private final String diagnostico;
+  /** Diagnostico del paciente. */
+  private final String diagnostico;
 
-    /**
-     * Veterinario del control
-     */
-    private final Persona veterinario;
+  /** Veterinario del control. */
+  private final Persona veterinario;
 
-    /**
-     * Constructor del control
+  /**
+     * Constructor del control.
      * @param fecha del control
      * @param fechaProximo control
      * @param temperatura del paciente
@@ -73,63 +59,71 @@ public class Control {
      * @param altura del paciente
      * @param diagnostico del paciente
      * @param veterinario del control
-     */
-    public Control(ZonedDateTime fecha, ZonedDateTime fechaProximo, double temperatura, double peso, double altura, String diagnostico, Persona veterinario) {
-        this.fecha = fecha;
-        this.fechaProximo = fechaProximo;
-        this.temperatura = temperatura;
-        this.peso = peso;
-        this.altura = altura;
-        this.diagnostico = diagnostico;
-        this.veterinario = veterinario;
-    }
+   * */
+  public Control(ZonedDateTime fecha, ZonedDateTime fechaProximo, double temperatura, double peso,
+      double altura, String diagnostico, Persona veterinario) {
+    this.fecha = fecha;
+    this.fechaProximo = fechaProximo;
+    this.temperatura = temperatura;
+    this.peso = peso;
+    this.altura = altura;
+    this.diagnostico = diagnostico;
+    this.veterinario = veterinario;
+  }
 
-    /**
-     * @return Fecha del control
-     */
-    public ZonedDateTime getFecha() {
-        return fecha;
-    }
+  /**
+   * Obtener la fecha de control.
+   * @return fecha
+   */
+  public ZonedDateTime getFecha() {
+    return fecha;
+  }
 
-    /**
-     * @return Fecha del proximo control
-     */
-    public ZonedDateTime getFechaProximo() {
-        return fechaProximo;
-    }
+  /**
+   * Obtener la fecha del proximo control.
+   * @return fechaProximo
+   */
+  public ZonedDateTime getFechaProximo() {
+    return fechaProximo;
+  }
 
-    /**
-     * @return Temperatura del paciente
-     */
-    public double getTemperatura() {
-        return temperatura;
-    }
+  /**
+   * Obtener temperatura del paciente.
+   * @return temperatura
+   */
+  public double getTemperatura() {
+    return temperatura;
+  }
 
-    /**
-     * @return Peso del paciente
-     */
-    public double getPeso() {
-        return peso;
-    }
+  /**
+   * Obtener peso del paciente.
+   * @return peso
+   */
+  public double getPeso() {
+    return peso;
+  }
 
-    /**
-     * @return Altura del paciente
-     */
-    public double getAltura() {
-        return altura;
-    }
+  /**
+   * Obtener altura del paciente.
+   * @return altura
+   */
+  public double getAltura() {
+    return altura;
+  }
 
-    /**
-     * @return Diagnosico del paciente
-     */
-    public String getDiagnostico() {
-        return diagnostico;
-    }
+  /**
+   * Obtener diagnostico del paciente.
+   * @return diagnostico
+   */
+  public String getDiagnostico() {
+    return diagnostico;
+  }
 
-    /**
-     * @return Veterinario
-     */
-    public Persona getVeterinario() {
-        return veterinario;
-    }
+  /**
+   * Obtener veterinario.
+   * @return veterinario
+   */
+  public Persona getVeterinario() {
+    return veterinario;
+  }
 }
