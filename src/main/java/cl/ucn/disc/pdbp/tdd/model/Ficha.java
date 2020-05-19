00f9d7b458b273ff88b94fedb5450006d5ae345a
Fabian Rojas <fnra027@gmail.com>
2020-05-19 10:16:36 -0400

    style(Ficha): add java google style format

--- src/main/java/cl/ucn/disc/pdbp/tdd/model/Ficha.java
+++ src/main/java/cl/ucn/disc/pdbp/tdd/model/Ficha.java
@@ -1,4 +1,3 @@
-
 /*
  * MIT License
  *
@@ -25,162 +24,162 @@
 
 package cl.ucn.disc.pdbp.tdd.model;
 
-
 import cl.ucn.disc.pdbp.tdd.dao.ZonedDateTimeType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
-
 import java.time.ZonedDateTime;
 
-@DatabaseTable (tableName = "ficha")
+@DatabaseTable(tableName = "ficha")
 public class Ficha {
 
-    @DatabaseField(generatedId = true)
-    private Long id;
-    /**
-     * Numero de ficha.
-     * Debe ser correlativo
-     */
-    @DatabaseField
-    private int numeroFicha;
+  /** Id del paciente. */
+  @DatabaseField(generatedId = true)
+  private Long id;
 
-    /**
-     * Nombre del paciente
-     */
-    @DatabaseField
-    private String nombrePaciente;
+  /** Numero de ficha. Debe ser correlativo */
+  @DatabaseField private int numeroFicha;
 
-    /**
-     * Especie del paciente
-     */
-    @DatabaseField
-    private String especie;
+  /** Nombre del paciente. */
+  @DatabaseField private String nombrePaciente;
 
-    /**
-     * Fecha de nacimiento del paciente
-     */
-    @DatabaseField(persisterClass = ZonedDateTimeType.class)
-    private ZonedDateTime fechaNacimiento;
+  /** Especie del paciente. */
+  @DatabaseField private String especie;
 
-    /**
-     * Raza del paciente
-     */
-    @DatabaseField
-    private String raza;
+  /** Fecha de nacimiento del paciente. */
+  @DatabaseField(persisterClass = ZonedDateTimeType.class)
+  private ZonedDateTime fechaNacimiento;
 
-    /**
-     * Sexo del paciente
-     * MACHO o HEMBRA
-     */
-    @DatabaseField
-    private Sexo sexo;
+  /** Raza del paciente. */
+  @DatabaseField private String raza;
 
-    /**
-     * Color del paciente
-     */
-    @DatabaseField
-    private String color;
+  /** Sexo del paciente MACHO o HEMBRA. */
+  @DatabaseField private Sexo sexo;
 
-    /**
-     * Tipo de paciente
-     * INTERNO o EXTERNO
-     */
-    @DatabaseField
-    private TipoPaciente tipoPaciente;
+  /** Color del paciente. */
+  @DatabaseField private String color;
 
-    @DatabaseField (foreign = true, foreignAutoRefresh = true)
-    private Persona duenio;
+  /** Tipo de paciente INTERNO o EXTERNO. */
+  @DatabaseField private TipoPaciente tipoPaciente;
 
-    /**
-     * Constructor vacio
-     */
-    public Ficha() {
-    }
+  /**
+   * Duenio del paciente.
+   * Clave foranea.
+   */
+  @DatabaseField(foreign = true, foreignAutoRefresh = true)
+  private Persona duenio;
 
-    /**
-     * Constructor de una ficha
-     * @param numeroFicha a usar
-     * @param nombrePaciente a usar
-     * @param especie del paciente
-     * @param fechaNacimiento del paciente
-     * @param raza del paciente
-     * @param sexo del paciente
-     * @param color del paciente
-     * @param tipoPaciente a usar
-     */
-    public Ficha(int numeroFicha, String nombrePaciente, String especie, ZonedDateTime fechaNacimiento, String raza, Sexo sexo, String color, TipoPaciente tipoPaciente, Persona duenio) {
-        this.numeroFicha = numeroFicha;
-        this.nombrePaciente = nombrePaciente;
-        this.especie = especie;
-        this.fechaNacimiento = fechaNacimiento;
-        this.raza = raza;
-        this.sexo = sexo;
-        this.color = color;
-        this.tipoPaciente = tipoPaciente;
-        this.duenio = duenio;
-    }
+  /** Constructor vacio. */
+  public Ficha() {}
 
-    /**
-     * @return el numero de ficha
-     */
-    public int getNumeroFicha() {
-        return numeroFicha;
-    }
+  /**
+   * Constructor de una ficha.   *
+   * @param numeroFicha a usar
+   * @param nombrePaciente a usar
+   * @param especie del paciente
+   * @param fechaNacimiento del paciente
+   * @param raza del paciente
+   * @param sexo del paciente
+   * @param color del paciente
+   * @param tipoPaciente a usar
+   */
+  public Ficha(
+      int numeroFicha,
+      String nombrePaciente,
+      String especie,
+      ZonedDateTime fechaNacimiento,
+      String raza,
+      Sexo sexo,
+      String color,
+      TipoPaciente tipoPaciente,
+      Persona duenio) {
+    this.numeroFicha = numeroFicha;
+    this.nombrePaciente = nombrePaciente;
+    this.especie = especie;
+    this.fechaNacimiento = fechaNacimiento;
+    this.raza = raza;
+    this.sexo = sexo;
+    this.color = color;
+    this.tipoPaciente = tipoPaciente;
+    this.duenio = duenio;
+  }
 
-    /**
-     * @return el nombre del paciente
-     */
-    public String getNombrePaciente() {
-        return nombrePaciente;
-    }
+  /**
+   * Obtener el numero de ficha.
+   * @return numeroFicha
+   */
+  public int getNumeroFicha() {
+    return numeroFicha;
+  }
 
-    /**
-     * @return la especie del paciente
-     */
-    public String getEspecie() {
-        return especie;
-    }
+  /**
+   * Obtener el nombre del paciente.
+   * @return nombrePaciente.
+   */
+  public String getNombrePaciente() {
+    return nombrePaciente;
+  }
 
-    /**
-     * @return la fecha de nacimiento del paciente
-     */
-    public ZonedDateTime getFechaNacimiento() {
-        return fechaNacimiento;
-    }
+  /**
+   * Obtener especie del paciente.
+   * @return especie
+   */
+  public String getEspecie() {
+    return especie;
+  }
 
-    /**
-     * @return la raza del paciente
-     */
-    public String getRaza() {
-        return raza;
-    }
+  /**
+   * Obtener la fecha de nacimiento del paciente.
+   * @return fechaNacimiento
+   */
+  public ZonedDateTime getFechaNacimiento() {
+    return fechaNacimiento;
+  }
 
-    /**
-     * @return el sexo del paciente
-     */
-    public Sexo getSexo() {
-        return sexo;
-    }
+  /**
+   * Obtener raza del paciente.
+   * @return raza
+   */
+  public String getRaza() {
+    return raza;
+  }
 
-    /**
-     * @return el color del paciente
-     */
-    public String getColor() {
-        return color;
-    }
+  /**
+   * Obtener el sexo del paciente.
+   * @return sexo
+   * */
+  public Sexo getSexo() {
+    return sexo;
+  }
 
-    /**
-     * @return el tipo de paciente
-     */
-    public TipoPaciente getTipoPaciente() {
-        return tipoPaciente;
-    }
+  /**
+   * Obtener el color del paciente.
+   * @return color
+   * */
+  public String getColor() {
+    return color;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  /**
+   * Obtener el tipo de paciente.
+   * @return tipoPaciente
+   * */
+  public TipoPaciente getTipoPaciente() {
+    return tipoPaciente;
+  }
 
-    public Persona getDuenio() {
-        return duenio;
-    }
+  /**
+   * Obtener id del paciente.
+   * @return id
+   */
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * Obtener duenio del paciente.
+   * @return duenio
+   */
+  public Persona getDuenio() {
+    return duenio;
+  }
 }
