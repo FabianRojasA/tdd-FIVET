d6b5fad271c2bc24a6fb0455d1ebdd119444fe2e
Fabian Rojas <fnra027@gmail.com>
2020-05-27 11:51:49 -0400

    feat(Application): api restful example to get time

---
+++ src/main/java/cl/ucn/disc/pdbp/tdd/Application.java
@@ -0,0 +1,95 @@
+/*
+ *
+ *  * MIT License
+ *  *
+ *  * Copyright (c) 2020 Fabian Rojas-Arce <fnra027@gmail.com>.
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in all
+ *  * copies or substantial portions of the Software.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  * SOFTWARE.
+ *
+ */
+
+package cl.ucn.disc.pdbp.tdd;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.javalin.Javalin;
+import io.javalin.core.util.RouteOverviewPlugin;
+import io.javalin.plugin.json.JavalinJson;
+import java.time.ZonedDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class Application {
+
+  private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+  private Application() {}
+
+  /**
+   * The main entry.
+   *
+   * @param args from console
+   */
+  public static void main(String[] args) {
+
+    // Gson configuration
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    JavalinJson.setFromJsonMapper(gson::fromJson);
+    JavalinJson.setToJsonMapper(gson::toJson);
+
+    // The Javalin application
+    log.debug("..Starting Javalin..");
+    Javalin javalin =
+        Javalin.create(
+            javalinConfig -> {
+
+              // Eneable extensive development
+              javalinConfig.enableDevLogging();
+
+              // Measure the time
+              javalinConfig.requestLogger(
+                  (ctx, executionTimeMs) -> {
+                    log.info("Served {} in {} ms.", ctx.fullUrl(), executionTimeMs);
+                    ctx.header("Server-Timing", "total;dur=" + executionTimeMs);
+                  });
+
+              // Eneable routes helper
+              javalinConfig.registerPlugin(new RouteOverviewPlugin("/routes"));
+            })
+            .start(7000);
+
+    // Shutdown hook!
+    Runtime.getRuntime()
+        .addShutdownHook(
+            new Thread(
+                () -> {
+                  log.debug("Stopping the server");
+                  javalin.stop();
+                  log.debug("The end");
+                }));
+
+    // A simple rut to show time
+    javalin.get(
+        "/",
+        ctx -> {
+          // Show the date
+          ctx.result("The Date: " + ZonedDateTime.now());
+        });
+  }
+}
